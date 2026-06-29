6539b1e40da3cf507dff7c609a3cd5b5a9e802d3
guutaramgmg-svg <guutaramgmg@gmail.com>
2026-06-30 00:25:40 +0900

    移動アニメーション追加

---
+++ Assets/Scripts/Move.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    [SerializeField] private Animator animator;
+
+    private Vector3 previousPosition;
+
+    private static readonly int XHash = Animator.StringToHash("X");
+    private static readonly int YHash = Animator.StringToHash("Y");
+
+    private Vector2 lastDirection = Vector2.down; // 初期は左向き
+
+//    private static readonly int SpeedHash = Animator.StringToHash("Speed");
+
+    void Start()
+    {
+        previousPosition = transform.position;
+    }
+
+    void Update()
+    {
+        Vector3 movement = transform.position - previousPosition;
+        if(movement.sqrMagnitude > 0.0001f)
+        {
+            lastDirection = movement.normalized;
+            animator.SetFloat(XHash, movement.x);
+            animator.SetFloat(YHash, movement.y);           
+        }
+        previousPosition = transform.position;
+    }
+
+}
